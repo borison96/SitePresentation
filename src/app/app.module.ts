f4d1e865f07d8e100dca40ae3a60b9d5f9e20492
Dziegain Boris <borisondziegain@hotmail.com>
2020-03-22 11:13:47 +0100

    Premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ImageTopComponent } from './image-top/image-top.component';
+import { MainBodyComponent } from './main-body/main-body.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      TopBarComponent,
+      ImageTopComponent,
+      MainBodyComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
